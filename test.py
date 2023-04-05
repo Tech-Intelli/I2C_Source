2ab15bfddcbb9c06ec0356bbb4e5260f0a59f192
Dipanjan Das <dasdipanjan04@gmail.com>
2023-04-05 23:40:52 +0200

    [WIP]: Use ImageCaption and ImageCompressor modules

--- test.py
+++ test.py
@@ -1,12 +1,20 @@
 import os
 import openai
+from imagecompressor.imagecompressor import ImageCompressor
+from imagecaption.imagecaption import ImageCaptionPipeLine
 
+compressed_image_path = ImageCompressor.compress("test.jpg", 10)
+image_pipeline = ImageCaptionPipeLine.get_image_caption_pipeline()
+text = image_pipeline("test.jpg")[0]['generated_text']
+
+content_poetry = input("Write what you want ChatGPT to do for you:\n")
+content_poetry = content_poetry + f" : {text}"
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
 responseJson = openai.ChatCompletion.create(
     model="gpt-3.5-turbo",
     messages=[
-        {"role": "system", "content": "Give 30 famous instagram accounts related to nature photography"},
+        {"role": "assistant", "content": content_poetry},
         #{"role": "user", "content": "Who won the world series in 2020?"},
         #{"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
     ]
