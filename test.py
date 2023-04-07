955182b30f81eafa67a11d0fc95cdfe4f30bed39
Dipanjan Das <dasdipanjan04@gmail.com>
2023-04-07 03:54:25 +0200

    use telegram bot to send picture

--- test.py
+++ test.py
@@ -16,17 +16,32 @@ responseJson = openai.ChatCompletion.create(
     model="gpt-3.5-turbo",
     messages=[
         {"role": "assistant", "content": content_poetry},
-        #{"role": "user", "content": "Who won the world series in 2020?"},
-        #{"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
     ]
 )
-
-print(responseJson["choices"][0]["message"]["content"])
 
 
 def writeResponse_to_json(responseJson):
     with open("responseJson.json", "a") as f:
         f.write(str(responseJson))
+    with open("output.txt", "a") as f:
+        f.write(responseJson["choices"][0]["message"]["content"])
 
 
 writeResponse_to_json(responseJson)
+caption = responseJson["choices"][0]["message"]["content"]
+
+import telegram
+import asyncio
+
+
+async def something(photo_path, caption=None):
+    chat_id = "1234556"
+    bot = telegram.Bot(token='Xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx')
+# Send a message with an image attachment
+    with open(photo_path, 'rb') as f:
+        await bot.send_photo(chat_id=chat_id, photo=f, caption=caption)
+
+
+photo_path = image_path
+
+asyncio.run(something(photo_path, caption))
