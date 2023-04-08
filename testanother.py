baa7d6993e054e916208461e382d1d0c80b96aaf
Dipanjan Das <dasdipanjan04@gmail.com>
2023-04-09 00:36:11 +0200

    Fix that code too.

--- testanother.py
+++ testanother.py
@@ -3,12 +3,11 @@
 import time
 import warnings
 warnings.filterwarnings("ignore")
-from cachemodel.diskcachedmodel import get_image_caption_pipeline
 
-for val in list:
-    start_time = time.time()
-    piplin = get_image_caption_pipeline(val)
-    #caption = piplin("test2.jpg")[0]['generated_text']
-    elapsed_time = time.time() - start_time
-    print(f"Caption generation took {elapsed_time} seconds.")
+start_time = time.time()
+from cachemodel.cachedmodel import get_image_caption_pipeline
+piplin = get_image_caption_pipeline("test.jpg")
+#caption = piplin("test2.jpg")[0]['generated_text']
+elapsed_time = time.time() - start_time
+print(f"Caption generation took {elapsed_time} seconds.")
 #print(caption)
