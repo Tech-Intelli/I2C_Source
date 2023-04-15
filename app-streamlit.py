97a1a7a7739997de1826badc212de4ed85be640a
Dipanjan Das <dasdipanjan04@gmail.com>
2023-04-15 03:26:30 +0200

    Add Gify and Generate Video Caption button

--- app-streamlit.py
+++ app-streamlit.py
@@ -1,3 +1,4 @@
+import base64
 import os
 import streamlit as st
 import generatecaption
@@ -11,14 +12,14 @@ CHATBOT = generatecaption.Chatbot(os.environ["OPENAI_API_KEY"])
 IMAGE_CAPTION_GENERATOR = generatecaption.ImageCaptionGenerator(CHATBOT)
 
 
-def generate_image_caption(image_path, caption_size=1):
+def generate_image_caption(image_path, caption_size="small"):
     responseJson, compressed_image_path = IMAGE_CAPTION_GENERATOR.\
-        generate_caption(image_path)
+        generate_caption(image_path, caption_size)
     caption = responseJson["choices"][0]["message"]["content"]
     return caption, compressed_image_path
 
 
-def generate_video_caption(video_path, caption_size=1):
+def generate_video_caption(video_path, caption_size="small"):
     video_caption_generator = generatecaption.VideoCaptionGenerator(
         CHATBOT,
         SceneDetector(),
@@ -28,10 +29,24 @@ def generate_video_caption(video_path, caption_size=1):
     return responseJson["choices"][0]["message"]["content"]
 
 
+def generate_interim_gif():
+    file_ = open("giphy.gif", "rb")
+    contents = file_.read()
+    data_url = base64.b64encode(contents).decode("utf-8")
+    file_.close()
+    gif_placeholder = st.empty()
+    gif_placeholder.markdown(
+        f'<img src="data:image/gif;base64,{data_url}" alt="explaistic gif">',
+        unsafe_allow_html=True,
+    )
+    return gif_placeholder
+
+
 def app():
     st.set_page_config(page_title=COMPANY_NAME, page_icon=COMPANY_LOGO)
     st.image(COMPANY_LOGO)
-    st.markdown('<style>img { display: block; margin: auto; }</style>', unsafe_allow_html=True)
+    st.markdown('<style>img { display: block; margin: auto; }</style>',
+                unsafe_allow_html=True)
     st.markdown(f"""
         <style>
             body {{
@@ -46,29 +61,35 @@ def app():
     uploaded_video = st.file_uploader(
         "Upload Video", type=["mp4", "mov"])
 
-    caption_size = st.slider("Caption Size", 1, 2, 3)
+    caption_size = st.select_slider(
+        'Caption Size',
+        options=['small', 'medium', 'large', 'very large', 'blog post'])
 
-    if st.button("Generate Caption"):
-        if uploaded_image is not None and uploaded_video is not None:
-            st.error("Please upload only one of either an image or a video.")
+    col1, col2, col3 = st.columns([1, 1, 0.80])
+    if col1.button("Generate Caption"):
+        if uploaded_image is None:
+            st.error("Please upload an image.")
         if uploaded_image is not None and uploaded_video is None:
-            with NamedTemporaryFile(dir='.', suffix='.jpg | \
-                                                    .jepg | .png') as f:
+            gif_placeholder = generate_interim_gif()
+            with NamedTemporaryFile(dir='.', suffix='.jpg |.jepg | .png') as f:
                 f.write(uploaded_image.getbuffer())
                 caption, compressed_image_path = generate_image_caption(
                     f.name, caption_size)
                 os.remove(compressed_image_path)
-                st.image(f.name)
+                gif_placeholder.empty()
                 st.success(caption)
-        elif uploaded_image is None and uploaded_video is not None:
+                st.image(f.name)
+    if col3.button("Generate Video Caption"):
+        if uploaded_video is None:
+            st.error("Please upload a video.")
+        else:
+            gif_placeholder = generate_interim_gif()
             with NamedTemporaryFile(dir='.', suffix='.mov | .mp4') as f:
                 f.write(uploaded_video.getbuffer())
                 caption = generate_video_caption(f.name, caption_size)
-                st.video(f.name)
+                gif_placeholder.empty()
                 st.success(caption)
-        elif uploaded_image is None and uploaded_video is None:
-            st.error("Please upload either an image or a video.")
-
+                st.video(f.name)
 
 if __name__ == "__main__":
     app()
