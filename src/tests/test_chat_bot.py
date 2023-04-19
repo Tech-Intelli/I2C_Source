420e55d152104edf02f55a091137687ca9928d4c
Dipanjan Das <dasdipanjan04@gmail.com>
2023-04-20 01:03:31 +0200

    Add unit test for generate caption Chatbot.get_response()

---
+++ src/tests/test_chat_bot.py
@@ -0,0 +1,47 @@
+"""
+Unit Test for ChatBot
+"""
+
+import os
+# pylint: disable=E0401
+from generate_caption import Chatbot
+
+
+def test_chat_bot():
+    """
+    Test the ChatBot response functionality.
+    """
+    chat_bot = Chatbot(os.environ["OPENAI_API_KEY"])
+
+    # Testcase: 1 Get a small response
+    response = chat_bot.get_response("Write a one line sentence on the Sun.")
+    response = response["choices"][0]["message"]["content"]
+    print(response)
+    assert response != ''
+
+    # Testcase: 2 Generate a numeric digit
+    response = chat_bot.get_response('''Generate a numeric digit.
+                                        No extra characters or text''')
+    response = response["choices"][0]["message"]["content"]
+
+    try:
+        response = int(response)
+    except ValueError:
+        assert False, "Invalid response, not a digit!"
+
+    # Testcase: 3 Generate an even number
+    response = chat_bot.get_response('''Generate an even number.
+                                        No extra characters or text''')
+    response = response["choices"][0]["message"]["content"]
+
+    try:
+        response = int(response)
+    except ValueError:
+        assert False, "Invalid response, not a number!"
+
+    assert response % 2 == 0
+
+    # Testcase: 4 First US President
+    response = chat_bot.get_response('''First US President''')
+    response = response["choices"][0]["message"]["content"]
+    assert "George Washington" in response
