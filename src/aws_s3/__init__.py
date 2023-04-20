358abe0bbbb4a70ee0b7c1bc7d940736672d4c73
Dipanjan Das <dasdipanjan04@gmail.com>
2023-04-21 00:34:54 +0200

    Create, upload and download S3

---
+++ src/aws_s3/__init__.py
@@ -0,0 +1,4 @@
+"""
+Image uplaod to and download from s3
+"""
+from .aws_s3 import AwsS3
---
+++ src/aws_s3/aws_s3.py
@@ -0,0 +1,40 @@
+"""
+Module for creating s3 buckets, upload and download images.
+"""
+import os
+import boto3
+
+
+class AwsS3:
+    """Create S3 bucket, Upload and Download image files.
+    """
+    s3 = boto3.client('s3')
+    region = 'eu-central-1'
+
+    @staticmethod
+    def create_bucket(s3_bucket_name):
+        """
+        Creates an AWS S§ bucket
+        """
+        AwsS3.s3.create_bucket(Bucket=s3_bucket_name, CreateBucketConfiguration={
+            'LocationConstraint': AwsS3.region})
+
+    @staticmethod
+    def upload_image_to_s3(image_path, s3_bucket_name):
+        """
+        Upload an image file to Amazon S3
+        """
+
+        key_name = os.path.basename(image_path)
+        AwsS3.s3.upload_file(image_path, s3_bucket_name, key_name)
+
+    @staticmethod
+    def download_image_from_s3(image_save_path, key_name, s3_bucket_name):
+        """_summary_
+
+        Args:
+            image_save_path (str): path/filename.jpg where the image will be saved
+            key_name (str): file name in s3 bucket
+            s3_bucket_name (str): Amazon S3 bucket name
+        """
+        AwsS3.s3.download_file(s3_bucket_name, key_name, image_save_path)
