91ddae172e136c2580787139478b6d795568ac57
Dipanjan Das <dasdipanjan04@gmail.com>
2023-04-29 04:30:02 +0200

    Implement if email id exists in table don' allow second regsitration

--- src/login/register_user/register_user.py
+++ src/login/register_user/register_user.py
@@ -5,6 +5,7 @@ Module that registers users into the database
 import uuid
 from ..database import TABLE
 from ..hash_password import hash_password
+from boto3.dynamodb.conditions import Key
 
 
 class RegisterUser:
@@ -23,7 +24,12 @@ class RegisterUser:
         hashed_password = hash_password(self.password)
 
         user_id = int(uuid.uuid4().int & (1 << 31)-1)
-
+        isExistingUser = TABLE.query(
+            KeyConditionExpression=Key('username').eq(self.username)
+        )
+        if len(isExistingUser['Items']) != 0 and \
+                isExistingUser['Items'][0]['username'] == self.username:
+            return 400
         response = TABLE.put_item(
             Item={
                 'username': self.username,
