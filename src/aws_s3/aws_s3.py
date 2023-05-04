1d3d7366f0c3ced1dd5eae3b2d9e0f6a80c2a89f
Dipanjan Das <dasdipanjan04@gmail.com>
2023-05-04 11:02:15 +0200

    Implement delete file from S3. Refactor by introducing exceptions and logging.

--- src/aws_s3/aws_s3.py
+++ src/aws_s3/aws_s3.py
@@ -18,8 +18,12 @@ class AwsS3:
         """
         Creates an AWS S3 bucket
         """
-        AwsS3.s3.create_bucket(Bucket=s3_bucket_name, CreateBucketConfiguration={
-            'LocationConstraint': AwsS3.region})
+        try:
+            AwsS3.s3.create_bucket(Bucket=s3_bucket_name, CreateBucketConfiguration={
+                'LocationConstraint': AwsS3.region})
+        except exceptions.ClientError as error:
+            logging.error(
+                'Error creating AWS S3 Bucket: %s due to error:\n%s', s3_bucket_name, error)
 
     @staticmethod
     def upload_file_to_s3(image_path, s3_bucket_name, key_name):
@@ -29,7 +33,8 @@ class AwsS3:
         try:
             AwsS3.s3.upload_file(image_path, s3_bucket_name, key_name)
         except exceptions.ClientError as error:
-            logging.error(error)
+            logging.error('Error uploading file: %s to %s due to error:\n%s',
+                          image_path, s3_bucket_name, error)
             return False
         return True
 
@@ -41,17 +46,41 @@ class AwsS3:
         try:
             AwsS3.s3.upload_fileobj(image_stream, s3_bucket_name, key_name)
         except exceptions.ClientError as error:
-            logging.error(error)
+            logging.error('Error uploading file: %s to %s due to error:\n%s',
+                          image_stream, s3_bucket_name, error)
             return False
         return True
 
     @staticmethod
     def download_image_from_s3(image_save_path, key_name, s3_bucket_name):
-        """_summary_
+        """Download image from S3 bucket.
 
         Args:
             image_save_path (str): path/filename.jpg where the image will be saved
             key_name (str): file name in s3 bucket
             s3_bucket_name (str): Amazon S3 bucket name
         """
-        AwsS3.s3.download_file(s3_bucket_name, key_name, image_save_path)
+        try:
+            AwsS3.s3.download_file(s3_bucket_name, key_name, image_save_path)
+        except exceptions.ClientError as error:
+            logging.error(
+                'Error downloading %s from %s due to error:\n%s', key_name, s3_bucket_name, error)
+            return False
+        return True
+
+    @staticmethod
+    def delete_file_from_s3(s3_bucket_name, key_name):
+        """
+        Delete a file from Amazon S3
+
+        Args:
+            s3_bucket_name (str): Amazon S3 bucket name
+            key_name (str): file name in s3 bucket
+        """
+        try:
+            AwsS3.s3.delete_object(s3_bucket_name, key_name)
+        except exceptions.ClientError as error:
+            logging.error(
+                'Error deleting %s from %s due to error:\n%s', key_name, s3_bucket_name, error)
+            return False
+        return True
