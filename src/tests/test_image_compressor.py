754845c18f0c3467829e1d389bc246526caf5a07
Dipanjan Das <dasdipanjan04@gmail.com>
2023-05-05 15:43:37 +0200

    Fix path.

--- src/tests/test_image_compressor.py
+++ src/tests/test_image_compressor.py
@@ -3,6 +3,7 @@ Unit Test Case Image Compression Method
 """
 
 import os
+from pathlib import Path
 # pylint: disable=E0401
 from image_compressor import ImageCompressor
 
@@ -12,7 +13,8 @@ def test_compress():
     Test Image Compression
     """
     # Test case 1: Compress image with default compression quality
-    image_path = 'tests/test_resources/images/test_image.jpg'
+    image_path = os.path.join(
+        Path.cwd(), "test_resources", "images", "test_image.jpg")
     compressed_path = ImageCompressor.compress(image_path)
     assert os.path.isfile(compressed_path)
     assert os.path.splitext(compressed_path)[1] == '.jpg'
@@ -20,7 +22,8 @@ def test_compress():
     os.remove(compressed_path)
 
     # Test case 2: Compress image with custom compression quality
-    image_path = 'tests/test_resources/images/test_image.jpg'
+    image_path = os.path.join(
+        Path.cwd(), "test_resources", "images", "test_image.jpg")
     compression_quality = 70
     compressed_path = ImageCompressor.compress(image_path, compression_quality)
     assert os.path.isfile(compressed_path)
@@ -29,7 +32,8 @@ def test_compress():
     os.remove(compressed_path)
 
     # Test case 3: Compress small image, should return same image path
-    image_path = 'tests/test_resources/images/small_image.jpg'
+    image_path = os.path.join(
+        Path.cwd(), "test_resources", "images", "small_image.jpg")
     compressed_path = ImageCompressor.compress(image_path)
     assert compressed_path == image_path
 
