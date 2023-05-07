b3527409de99d45d4d70454fcebb9d8a429cc005
Dipanjan Das <dasdipanjan04@gmail.com>
2023-05-08 01:28:10 +0200

    Implement forget password

--- src/login/authenticate_user/autheticate_user.py
+++ src/login/authenticate_user/autheticate_user.py
@@ -28,3 +28,37 @@ class AuthenticateUser:
             return False
         hashed_password = response['Items'][0]['password']
         return (len(response['Items']) != 0) and (hash_password(self.password) == hashed_password)
+
+
+class ForgetPassword:
+    """
+    Forget Password with email address
+    """
+
+    def __init__(self, username):
+        self.username = username
+
+    def forget_password(self, new_password):
+        """Forget password
+        """
+        response = TABLE.query(
+            KeyConditionExpression=Key('username').eq(self.username)
+        )
+        if not response['Items']:
+            return False
+        user_id = response['Items'][0]['user_id']
+        new_password = hash_password(new_password)
+        response = TABLE.update_item(
+            Key={
+                "username": self.username,
+                "user_id": user_id,
+            },
+            UpdateExpression="SET #password = :new_password",
+            ExpressionAttributeNames={
+                "#password": "password"
+            },
+            ExpressionAttributeValues={
+                ":new_password": new_password
+            }
+        )
+        return True
