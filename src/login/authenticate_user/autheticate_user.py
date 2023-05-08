972795f44066c99f39d6f0bb8435dceb44484a81
Dipanjan Das <dasdipanjan04@gmail.com>
2023-05-08 19:11:04 +0200

    Get user Id

--- src/login/authenticate_user/autheticate_user.py
+++ src/login/authenticate_user/autheticate_user.py
@@ -29,6 +29,19 @@ class AuthenticateUser:
         hashed_password = response['Items'][0]['password']
         return (len(response['Items']) != 0) and (hash_password(self.password) == hashed_password)
 
+    def get_user_id(self):
+        """Get the user id from DynamoDB
+
+        Returns:
+            user_id: string
+        """
+        response = TABLE.query(
+            KeyConditionExpression=Key('username').eq(self.username)
+        )
+        if not response['Items']:
+            return "No User Found"
+        return response['Items'][0]['user_id']
+
 
 class ForgetPassword:
     """
