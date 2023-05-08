f5e0fc5a761b485435c86a9ffe4195480f35ef26
Dipanjan Das <dasdipanjan04@gmail.com>
2023-05-08 16:44:59 +0200

    Change app_endpoints so that image and video can be uploaed as a file, caption can be generated from a generalised pov, if it's an image image caption will be generated, if it's a video video caption will be generated.

--- src/app_endpoints.py
+++ src/app_endpoints.py
@@ -5,6 +5,7 @@ Flask end point for the caption generation
 import os
 import string
 import random
+import pathlib
 from pathlib import Path
 from datetime import datetime
 from functools import wraps
@@ -16,6 +17,7 @@ from login.register_user import RegisterUser
 from login.authenticate_user import AuthenticateUser
 from login.authenticate_user import ForgetPassword
 
+ALLOWED_FILE_EXTENSIONS = {'png', 'jpg', 'jpeg', 'mov', 'avi', 'mp4'}
 ALLOWED_IMAGE_FILE_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 ALLOWED_VIDEO_FILE_EXTENSIONS = {'mov', 'avi', 'mp4'}
 S3_BUCKET_NAME = 'explaisticbucket'
@@ -76,8 +78,9 @@ def login_user():
     password = data.get('password')
     if not email or not password:
         return jsonify({"error": "Email and Password must be provided to login"}), 400
-    is_user_authticated = AuthenticateUser(email, password).authenticate_user()
-    if is_user_authticated:
+    is_user_authenticated = AuthenticateUser(
+        email, password).authenticate_user()
+    if is_user_authenticated:
         session['email'] = email
         return jsonify({"Success": "User is authenticated and logged in"}), 200
     return jsonify({"Error": "Login failed, please check your email and password"}), 400
@@ -118,7 +121,7 @@ def generate_random_filename(filename, extension):
     return filename
 
 
-def allowed_image_file(filename):
+def allowed_file(filename):
     """Checks whether the image file extension is allowed
 
     Args:
@@ -128,137 +131,72 @@ def allowed_image_file(filename):
         bool: returns True if image file extension is allowed
     """
     return '.' in filename and \
-        filename.rsplit('.', 1)[1].lower() in ALLOWED_IMAGE_FILE_EXTENSIONS
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_FILE_EXTENSIONS
 
 
-def allowed_video_file(filename):
-    """Checks whether the video file extension is allowed
-
-    Args:
-        filename (str): _description_
-
-    Returns:
-        bool: returns True if the video file extension is allowed
-    """
-    return '.' in filename and \
-        filename.rsplit('.', 1)[1].lower() in ALLOWED_VIDEO_FILE_EXTENSIONS
-
-
-@app.route('/upload_image', methods=['POST'])
+@app.route('/upload_file', methods=['POST'])
 @login_required
-def upload_image():
-    """Uploads an image to the S3 Bucket.
-
-    Returns:
-        JSON: if uploaded successfully returns JSON indicating sucess or failure
-    """
-    image_path = request.files.get('image')
-    if image_path and allowed_image_file(os.path.basename(image_path.filename)):
-        image_file_name = os.path.basename(image_path.filename)
-        file_name = image_file_name.rsplit('.', 1)[0]
-        file_extension = image_file_name.rsplit('.', 1)[1].lower()
-        image_file_name = generate_random_filename(file_name, file_extension)
-        session['image_file_name'] = image_file_name
-        response = AwsS3.upload_file_object_to_s3(
-            image_path.stream, S3_BUCKET_NAME, image_file_name)
-        if response:
-            return jsonify({"Uploaded Successfully": True})
-        return jsonify({"Upload Failed": False})
-    return jsonify({"No Image File Selected": False})
-
-
-@app.route('/upload_video', methods=['POST'])
-@login_required
-def upload_video():
-    """Uploads a video to the S3 bucket.
+def upload_file():
+    """Uploads a file to the S3 Bucket.
 
     Returns:
         JSON: if uploaded successfully returns JSON indicating success or failure
     """
-    video_path = request.files.get('video')
-    if video_path and allowed_video_file(os.path.basename(video_path.filename)):
-        video_file_name = os.path.basename(video_path.filename)
-        file_name = video_file_name.rsplit('.', 1)[0]
-        file_extension = video_file_name.rsplit('.', 1)[1].lower()
-        video_file_name = generate_random_filename(file_name, file_extension)
-        session['video_file_name'] = video_file_name
+    file_path = request.files.get('file')
+    if file_path and allowed_file(os.path.basename(file_path.filename)):
+        file_name = os.path.basename(file_path.filename)
+        file_name = file_name.rsplit('.', 1)[0]
+        file_extension = file_path.filename.rsplit('.', 1)[1].lower()
+        file_name = generate_random_filename(file_name, file_extension)
+        session['file_name'] = file_name
         response = AwsS3.upload_file_object_to_s3(
-            video_path, S3_BUCKET_NAME, video_file_name)
+            file_path.stream, S3_BUCKET_NAME, file_name)
         if response:
             return jsonify({"Uploaded Successfully": True})
-        return jsonify({"Uploaded Failed": False})
-    return jsonify({"No video file selected": False})
+        return jsonify({"Upload Failed": False})
+    return jsonify({"No File Selected": False})
 
 
-@app.route('/generate_image_caption', methods=['GET'])
+@app.route('/generate_image_video_caption', methods=['GET'])
 @login_required
-def generate_image_caption():
+def generate_image_video_caption():
     """Generates an image caption
 
     Returns:
         JSON: JSON representation of caption
     """
-    image_file_name = session.get('image_file_name', None)
-    image_save_path = os.path.join(Path.cwd(), image_file_name)
+    file_name = session.get('file_name', None)
+    file_save_path = os.path.join(Path.cwd(), file_name)
     AwsS3.download_file_from_s3(
-        image_save_path, image_file_name, S3_BUCKET_NAME)
+        file_save_path, file_name, S3_BUCKET_NAME)
     caption_size = request.args.get('caption_size', "small")
     context = request.args.get('context', "")
     style = request.args.get('style', 'cool')
     num_hashtags = request.args.get('num_hashtags', 0)
     tone = request.args.get('tone', 'casual')
     social_media = request.args.get('social_media', 'instagram')
-    response_json, _ = IMAGE_CAPTION_GENERATOR.generate_caption(
-        image_save_path, caption_size, context, style, num_hashtags, tone, social_media)
+    file_extension = pathlib.Path(
+        file_save_path).suffix.rsplit('.', 1)[1].lower()
+    response_json = None
+    if file_extension in ALLOWED_IMAGE_FILE_EXTENSIONS:
+        response_json, _ = IMAGE_CAPTION_GENERATOR.generate_caption(
+            file_save_path, caption_size, context, style, num_hashtags, tone, social_media)
+    elif file_extension in ALLOWED_VIDEO_FILE_EXTENSIONS:
+        response_json = VIDEO_CAPTION_GENERATOR.generate_caption(
+            file_save_path, caption_size, context, style, num_hashtags, tone, social_media)
     if response_json is not None:
         return jsonify({"Caption": response_json["choices"][0]["message"]["content"]})
     return jsonify({"Caption": "Couldn't find a caption"})
 
 
-@app.route("/retry_image_caption", methods=["GET"])
+@app.route("/retry_image_video_caption", methods=["GET"])
 @login_required
-def retry_image_caption():
+def retry_image_video_caption():
     """Retry captioning
     Returns:
-        generate_image_caption
+        generate_image_video_caption
     """
-    return generate_image_caption()
-
-
-@app.route('/generate_video_caption', methods=['GET'])
-@login_required
-def generate_video_caption():
-    """Generates a video caption
-
-    Returns:
-        JSON: JSON representation of caption
-    """
-    video_file_name = session.get('video_file_name', None)
-    video_save_path = os.path.join(Path.cwd(), video_file_name)
-    AwsS3.download_file_from_s3(
-        video_save_path, video_file_name, S3_BUCKET_NAME)
-    caption_size = request.args.get('caption_size', "small")
-    context = request.args.get('context', "")
-    style = request.args.get('style', 'cool')
-    num_hashtags = request.args.get('num_hashtags', 0)
-    tone = request.args.get('tone', 'casual')
-    social_media = request.args.get('social_media', 'instagram')
-    response_json = VIDEO_CAPTION_GENERATOR.generate_caption(
-        video_save_path, caption_size, context, style, num_hashtags, tone, social_media)
-    AwsS3.delete_file_from_s3(S3_BUCKET_NAME, video_file_name)
-    if response_json is not None:
-        return jsonify({"Caption": response_json["choices"][0]["message"]["content"]})
-    return jsonify({"Caption": "Couldn't find a caption"})
-
-
-@app.route("/retry_video_caption", methods=["GET"])
-@login_required
-def retry_video_caption():
-    """Retry captioning
-    Returns:
-        generate_video_caption
-    """
-    return generate_video_caption()
+    return generate_image_video_caption()
 
 
 if __name__ == '__main__':
