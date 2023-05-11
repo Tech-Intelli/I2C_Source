4859a62d729f3b61b9a3af88a7f0ccf48113ce5c
Dipanjan Das <dasdipanjan04@gmail.com>
2023-05-12 00:40:42 +0200

    [WIP]: Send email verfication link via email.

--- src/login/register_user/register_user.py
+++ src/login/register_user/register_user.py
@@ -2,7 +2,11 @@
 Module that registers users into the database
 """
 
+import os
+import ssl
 import uuid
+import smtplib
+from email.message import EmailMessage
 # pylint: disable=E0401
 from boto3.dynamodb.conditions import Key
 from ..database import TABLE
@@ -20,6 +24,21 @@ class RegisterUser:
         self.username = username
         self.password = password
 
+    def send_email(self, url, unique_id):
+        """Send and email to the user to verify the email address"""
+        msg = EmailMessage()
+        msg.set_content(
+            f"Please click the following link to verify your email:\n\n{url}/{unique_id}")
+        msg["Subject"] = "Email Verification"
+        msg["From"] = os.environ["EMAIL_ADDRESS"]
+        msg["To"] = self.username
+        context = ssl.create_default_context()
+        with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+            server.login(os.environ["EMAIL_ADDRESS"],
+                         os.environ["EMAIL_PASSWORD"])
+            server.sendmail(os.environ["EMAIL_ADDRESS"],
+                            self.username, msg.as_string())
+
     def register_user(self):
         """
         Register the user to the database
@@ -33,6 +52,8 @@ class RegisterUser:
         if len(is_existing_user['Items']) != 0 and \
                 is_existing_user['Items'][0]['username'] == self.username:
             return 400
+        unique_id = str(uuid.uuid4())
+        self.send_email("http://localhost:9000/somevalue/", unique_id)
         response = TABLE.put_item(
             Item={
                 'username': self.username,
