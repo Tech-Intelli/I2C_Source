3d8cc5f9e6374ba23993043031eca7a5a01247cc
Dipanjan Das <dasdipanjan04@gmail.com>
2023-05-14 00:14:32 +0200

    Fix: Pytest, add a new testcase to verify user

--- src/tests/test_authenticate_user.py
+++ src/tests/test_authenticate_user.py
@@ -12,9 +12,14 @@ def test_authenticate_user_correct_password(mocker):
     """
     username = "username"
     password = "password"
+    verified = True
     hashed_password = hash_password(password)
     mocker.patch.object(TABLE, 'query', return_value={
-                        'Items': [{username: 'username', password: hashed_password}]})
+                        'Items': [{
+                            'username': username,
+                            'password': hashed_password,
+                            'verified': verified}]
+                        })
     auth = AuthenticateUser(username, password)
     assert auth.authenticate_user() is True
 
@@ -25,9 +30,33 @@ def test_authenticate_user_incorrect_password(mocker):
     """
     username = "username"
     password = "password"
+    verified = True
     hashed_password = hash_password("Incorrect")
     mocker.patch.object(TABLE, 'query', return_value={
-                        'Items': [{username: 'username', password: hashed_password}]})
+                        'Items': [{
+                            'username': username,
+                            'password': hashed_password,
+                            'verified': verified
+                        }]
+                        })
+    auth = AuthenticateUser(username, password)
+    assert auth.authenticate_user() is False
+
+
+def test_authenticate_user_not_verified(mocker):
+    """
+    Test authenticate_user when password is incorrect.
+    """
+    username = "username"
+    password = "password"
+    verified = False
+    hashed_password = hash_password("password")
+    mocker.patch.object(TABLE, 'query', return_value={
+                        'Items': [{
+                            'username': username,
+                            'password': hashed_password,
+                            'verified': verified}]
+                        })
     auth = AuthenticateUser(username, password)
     assert auth.authenticate_user() is False
 
