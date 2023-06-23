7992c5c610d01c165321c9850cd4aefd2e95cb75
Agnik Bakshi <bakshi.agnik@gmail.com>
2023-06-24 02:10:46 +0530

    Added Location Feature

--- frontend/src/components/UploadFile/UploadFile.jsx
+++ frontend/src/components/UploadFile/UploadFile.jsx
@@ -1,24 +1,88 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { Circles } from "react-loader-spinner";
-
 import "./UploadFile.css";
+const apiKey = process.env.REACT_APP_MAP_API;
+const mapApiJs = 'https://maps.googleapis.com/maps/api/js';
+const geocodeJson = 'https://maps.googleapis.com/maps/api/geocode/json';
+
+function loadAsyncScript(src) {
+  return new Promise(resolve => {
+    const script = document.createElement("script");
+    Object.assign(script, {
+      type: "text/javascript",
+      async: true,
+      src
+    })
+    script.addEventListener("load", () => resolve(script));
+    document.head.appendChild(script);
+  })
+}
+
+const extractAddress = (place) => {
+
+  const address = {
+    city: "",
+    state: "",
+    zip: "",
+    country: "",
+    plain() {
+      const city = this.city ? this.city + ", " : "";
+      const zip = this.zip ? this.zip + ", " : "";
+      const state = this.state ? this.state + ", " : "";
+      return city + zip + state + this.country;
+    }
+  }
+
+  if (!Array.isArray(place?.address_components)) {
+    return address;
+  }
+
+  place.address_components.forEach(component => {
+    const types = component.types;
+    const value = component.long_name;
+
+    if (types.includes("locality")) {
+      address.city = value;
+    }
+
+    if (types.includes("administrative_area_level_2")) {
+      address.state = value;
+    }
+
+    if (types.includes("postal_code")) {
+      address.zip = value;
+    }
+
+    if (types.includes("country")) {
+      address.country = value;
+    }
+
+  });
+
+  return address;
+}
+
+
 
 export const UploadFile = (props) => {
+  
   const token = localStorage.getItem("token");
   console.log(token);
   const inputref = useRef();
   const navigate = useNavigate();
-  const wrapperRef = useRef(null);
+  const wrapperRef = useRef();
   const [fileList, setFileList] = useState();
   const [file, setfile] = useState();
   const [fileType, setFileType] = useState();
   const [loading,setLoading] = useState(false);
-
+  
   const onDragEnter = () => wrapperRef.current.classList.add("dragover");
   const onDragLeave = () => wrapperRef.current.classList.remove("dragover");
   const onDrop = () => wrapperRef.current.classList.remove("dragover");
+  
+
   const onFileDrop = (e) => {
     const newFile = e.target.files[0];
     const input_file = e.target.files[0];
@@ -41,7 +105,6 @@ export const UploadFile = (props) => {
     setfile();
     setFileList();
   };
-
   const handleClick = () => {
     setLoading(true);
      const formData = new FormData();
@@ -68,6 +131,70 @@ export const UploadFile = (props) => {
 
    
   };
+  const searchInput = useRef(null);
+  const [address, setAddress] = useState({});
+
+
+  // init gmap script
+  const initMapScript = () => {
+    // if script already loaded
+    if(window.google) {
+      return Promise.resolve();
+    }
+    console.log(apiKey)
+    const src = `${mapApiJs}?key=${apiKey}&libraries=places&v=weekly`;
+    return loadAsyncScript(src);
+  }
+
+  // do something on address change
+  const onChangeAddress = (autocomplete) => {
+    const place = autocomplete.getPlace();
+    setAddress(extractAddress(place));
+  }
+
+  // init autocomplete
+  const initAutocomplete = () => {
+    if (!searchInput.current) return;
+
+    const autocomplete = new window.google.maps.places.Autocomplete(searchInput.current);
+    autocomplete.setFields(["address_component", "geometry"]);
+    autocomplete.addListener("place_changed", () => onChangeAddress(autocomplete));
+
+  }
+
+
+  const reverseGeocode = ({ latitude: lat, longitude: lng}) => {
+    const url = `${geocodeJson}?key=${apiKey}&latlng=${lat},${lng}`;
+    searchInput.current.value = "Getting your location...";
+    fetch(url)
+        .then(response => response.json())
+        .then(location => {
+          const place = location.results[0];
+          const _address = extractAddress(place);
+          setAddress(_address);
+          searchInput.current.value = _address.plain();
+        })
+  }
+
+
+  const findMyLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+        reverseGeocode(position.coords)
+      })
+    }
+  }
+
+
+
+
+
+  // load map script after mounted
+  useEffect(() => {
+    initMapScript().then(() => initAutocomplete())
+  }, []);
+
+
   return (
     <>
       <div>
@@ -84,16 +211,26 @@ export const UploadFile = (props) => {
           <div className="content-page1">
             <div className="innerContent-page1">
               <p className="steps">Step 1 :Upload Files</p>
-              <div>
-                <p className="label">
-                  Add Context <span>(optional)</span>
-                </p>
-                <input
-                  placeholder="Tell us something about your memory"
-                  className="context-page1"
-                ></input>
+              <div className="context-location-container">
+                <div>
+                  <p className="label">
+                    Add Context <span>(optional)</span>
+                  </p>
+                  <input
+                    type = "text"
+                    placeholder="Tell us something about your memory"
+                    className="inputs context-page1"
+                  ></input>
+                </div>
+                <div>
+                  <p className="label">
+                    Add Location:
+                  </p>
+                  <div className="search">
+                      <input ref={searchInput} type="text" placeholder="Search location...." />
+                  </div>
+                </div>
               </div>
-
               <div
                 ref={wrapperRef}
                 className="drop-file-input"
@@ -161,7 +298,7 @@ export const UploadFile = (props) => {
           <div className="footer-page1"></div>
         </section>
         <div className="images-page1">
-          <svg
+        <svg
             width="100%"
             height="100%"
             viewBox="0 0 683 953"
