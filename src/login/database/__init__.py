ef04c50961d207d015c37b69dcf4f7d74e30f044
Dipanjan Das <dasdipanjan04@gmail.com>
2024-07-04 13:01:50 +0200

    Delete login user management. This had some dependency on boto3 and awss3.

--- src/login/database/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-Module that returns the DynamoDB and the Table
-"""
-
-from .database import DYNAMODB, TABLE, get_user_id
--- src/login/user_db/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-Package for creating a user registration table.
-"""
-
-from .create_db import CreateDatabase
