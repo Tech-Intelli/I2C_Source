0cfedc5bc016ab60d13e03dd7956c14421ec5015
sayantanroy47 <sayantanroy47@gmail.com>
2024-07-05 11:06:42 +0200

    fix and black

--- docs/conf.py
+++ docs/conf.py
@@ -18,8 +18,6 @@ current_dir = os.path.dirname(__file__)
 projectPath = os.path.abspath(os.path.join(current_dir, "../src"))
 sys.path.append(projectPath)
 
-from generate_caption import ImageCaptionGenerator
-from image_pipeline import Blip2Pipeline
 year = datetime.datetime.now().year
 
 project = "PulseMan"
@@ -34,12 +32,12 @@ autoapi_dirs = ["../src"]
 
 extensions = [
     "autoapi.extension",
-    "sphinx.ext.autodoc",    
+    "sphinx.ext.autodoc",
     "sphinx.ext.viewcode",
     "sphinx.ext.napoleon",
     "sphinx.ext.ifconfig",
     "sphinx_copybutton",
-    #"sphinx.ext.autosummary",
+    # "sphinx.ext.autosummary",
 ]
 
 autodoc_typehints = "description"
@@ -57,29 +55,29 @@ autoapi_options = [
 
 # html_logo = '../resources/logo.png'
 
-#pygments_style = "stata-dark"
+# pygments_style = "stata-dark"
 
-#html_logo = "https://pydata.org/wp-content/uploads/2019/06/pydata-logo-final.png"
+# html_logo = "https://pydata.org/wp-content/uploads/2019/06/pydata-logo-final.png"
 html_title = "PulseMan Docs"
 
-#Using https://pydata-sphinx-theme.readthedocs.io/en/stable/index.html
+# Using https://pydata-sphinx-theme.readthedocs.io/en/stable/index.html
 
 html_theme_options = {
     "use_edit_page_button": True,
     "back_to_top_button": True,
-    #"repository_url": "https://github.com/Tech-Intelli/I2C_Source",
-    #"use_repository_button": True, #for book theme
-    #"max_navbar_depth": 2,         #for book theme
-    #"collapse_navbar": True,       #for book theme
+    # "repository_url": "https://github.com/Tech-Intelli/I2C_Source",
+    # "use_repository_button": True, #for book theme
+    # "max_navbar_depth": 2,         #for book theme
+    # "collapse_navbar": True,       #for book theme
     "pygments_light_style": "tango",
     "pygments_dark_style": "monokai",
     "primary_sidebar_end": ["indices.html"],
-    "secondary_sidebar_items": ["page-toc", "sourcelink"],  
+    "secondary_sidebar_items": ["page-toc", "sourcelink"],
     "show_toc_level": 1,
     "show_nav_level": 1,
     "navigation_depth": 2,
     "external_links": [
-      {"name": "Source Code", "url": "https://github.com/Tech-Intelli/I2C_Source"}      
+        {"name": "Source Code", "url": "https://github.com/Tech-Intelli/I2C_Source"}
     ],
     "icon_links": [
         {
@@ -91,7 +89,7 @@ html_theme_options = {
             "name": "Twitter",
             "url": "https://twitter.com/<your-handle>",
             "icon": "fa-brands fa-square-twitter",
-        }
+        },
     ],
 }
 
