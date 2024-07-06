44832f28f62d00d9ab0a42b6c6434158a53592ea
Dipanjan Das <dasdipanjan04@gmail.com>
2024-07-06 20:42:31 +0200

    Fix init file

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,5 +1,5 @@
 from .generate_directory_structure import generate_directory_structure
-from .generate_gif_placeholder import generate_gif_placeholder
+from .generate_gif_placeholder import generate_interim_gif
 from .hashtag import Hashtag
 from .llm_chatbot import LLMChatbot
 from .prompt import Prompt
