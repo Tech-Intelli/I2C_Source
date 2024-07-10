5eeb76c19670acecbc5d6fa607e46f57be306f74
sayantanroy47 <sayantanroy47@gmail.com>
2024-07-10 16:19:44 +0200

    test

--- tests/test_usage.py
+++ tests/test_usage.py
@@ -307,7 +307,7 @@ class TestImageProcessing(unittest.TestCase):
 
             
     @time_it
-    def test_color_splash(self):
+    def test_color_splash(self):  
         tc = (255, 0, 0)
         img = apply_color_splash(self.image, tc)  
         output_path = os.path.join(self.temp_dir, "output_color_splash.jpg")
