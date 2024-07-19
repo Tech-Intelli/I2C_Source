2c7c92b8511063d70ac4e03100aba3309322148b
Dipanjan Das <dasdipanjan04@gmail.com>
2024-07-19 20:39:09 +0200

    Add all pure virtual functions to the interface.

--- src_cpp/inference/interface/include/InferenceIntreface.h
+++ src_cpp/inference/interface/include/InferenceIntreface.h
@@ -6,19 +6,23 @@
  * the getImageCaption and loadModel methods. The 'collection' attribute is
  * protected and can be accessed by subclasses.
  */
-#ifndef INFERENCE_ABSTRACT_H
-#define INFERENCE_ABSTRACT_H
+#ifndef INFERENCE_INTERFACE_H
+#define INFERENCE_INTERFACE_H
 
 #include <string>
 
-class InferenceAbstract {
+class InterfaceInterface {
 public:
-    virtual ~InferenceAbstract() {}
+    explicit InterfaceInterface(const std::string& collection)
+        : m_collection(collection) {}
+    virtual ~InterfaceInterface() {}
     virtual std::string getImageCaption(const std::string& imagePath) = 0;
     virtual void loadModel() = 0;
-
+    virtual std::string getDevice() const = 0;
+    virtual void loadImage(const std::string& image_path) = 0; 
+    virtual void getImageCaptionPipeline(const std::string& image_path) = 0;
 protected:
-    std::string collection;
+    std::string m_collection;
 };
 
-#endif
+#endif // INFERENCE_INTERFACE_H
