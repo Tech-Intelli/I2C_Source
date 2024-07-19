a2fb74207f96adfc8249f319bb23187d22c4019c
Dipanjan Das <dasdipanjan04@gmail.com>
2024-07-19 18:25:30 +0200

    Add doc string.

--- src_cpp/inference/interface/include/InferenceIntreface.h
+++ src_cpp/inference/interface/include/InferenceIntreface.h
@@ -1,6 +1,18 @@
 #include <string>
 #include <memory>
 
+/**
+ * @brief Class representing an interface for inference operations.
+ * 
+ * This class provides methods for getting image captions and loading models for inference.
+ * 
+ * Public Methods:
+ *  - std::string getImageCaption(const std::string& image_path): Retrieves a caption for the specified image.
+ *  - void loadModel(): Loads the model for inference.
+ * 
+ * Protected Attributes:
+ *  - std::string m_collection: A string representing the collection used by the interface.
+ */
 class InferenceInterface
 {
 public:
