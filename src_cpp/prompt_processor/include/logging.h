c75efcec5c54927c7fd3f6ab7e4f6ddd5d2f086a
sayantanroy47 <sayantanroy47@gmail.com>
2024-07-19 11:49:51 +0200

    remove inheritance completely

--- src_cpp/prompt_processor/include/logging.h
+++ src_cpp/prompt_processor/include/logging.h
@@ -3,6 +3,7 @@
 #include <spdlog/spdlog.h>
 #include <spdlog/sinks/stdout_color_sinks.h>
 #include <memory>
+#include <string>
 
 /**
  * @file Log.h
@@ -31,120 +32,63 @@
 class Log
 {
 public:
-    static Log &getInstance()
-    {
-        static Log instance;
-        return instance;
-    }
-
-    /**
-     * Initializes the logger with the specified logger name and sets up the console sink.
-     *
-     * @param logger_name The name of the logger. Defaults to "logger".
-     * @returns None
-     *
-     * @throws None
-     */
     static void init(const std::string &logger_name = "logger")
     {
+        // Create a console sink with color support
         auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
         console_sink->set_level(spdlog::level::trace);
-        console_sink->set_pattern("[%Y-%m-%d %H:%M:%S.%e] [%s:%#] %!() [%^%-5l%$] [t%t] %v");
+        console_sink->set_pattern("[%Y-%m-%d %H:%M:%S.%e] [%s:%#] [%^%-5l%$] [T%t] %v");
 
+        // Create a logger with the specified name
         auto logger = std::make_shared<spdlog::logger>(logger_name, console_sink);
         logger->set_level(spdlog::level::trace);
+
+        // Set the default logger
         spdlog::set_default_logger(logger);
     }
 
-    /**
-     * Logs a trace message with the specified format and arguments.
-     *
-     * @tparam Args the types of the arguments
-     * @param fmt the format string
-     * @param args the arguments to be formatted
-     *
-     * @throws None
-     */
+    // Log methods
     template <typename... Args>
     void trace(const char *fmt, const Args &...args)
     {
         spdlog::trace(fmt, args...);
     }
 
-    /**
-     * Logs a debug message with the specified format and arguments.
-     *
-     * @tparam Args the types of the arguments
-     * @param fmt the format string
-     * @param args the arguments to be formatted
-     *
-     * @throws None
-     */
     template <typename... Args>
     void debug(const char *fmt, const Args &...args)
     {
         spdlog::debug(fmt, args...);
     }
 
-    /**
-     * Logs an informational message with the specified format and arguments.
-     *
-     * @tparam Args the types of the arguments
-     * @param fmt the format string
-     * @param args the arguments to be formatted
-     *
-     * @throws None
-     */
     template <typename... Args>
     void info(const char *fmt, const Args &...args)
     {
         spdlog::info(fmt, args...);
     }
 
-    /**
-     * Logs a warning message with the specified format and arguments.
-     *
-     * @param fmt the format string
-     * @param args the arguments to be formatted
-     *
-     * @throws None
-     */
     template <typename... Args>
     void warn(const char *fmt, const Args &...args)
     {
         spdlog::warn(fmt, args...);
     }
 
-    /**
-     * Logs an error message with the specified format and arguments.
-     *
-     * @param fmt the format string
-     * @param args the arguments to be formatted
-     *
-     * @return None
-     *
-     * @throws None
-     */
     template <typename... Args>
     void error(const char *fmt, const Args &...args)
     {
         spdlog::error(fmt, args...);
     }
 
-    /**
-     * A description of the entire C++ function.
-     *
-     * @param fmt the format string
-     * @param args the arguments to be formatted
-     *
-     * @return None
-     *
-     * @throws None
-     */
     template <typename... Args>
     void critical(const char *fmt, const Args &...args)
     {
         spdlog::critical(fmt, args...);
+    }
+
+    // Macro for convenience
+    static Log &getInstance()
+    {
+        static Log instance;
+        return instance;
     }
 
 private:
@@ -153,4 +97,5 @@ private:
     Log &operator=(const Log &) = delete;
 };
 
+// Macro for convenience
 #define log Log::getInstance()
