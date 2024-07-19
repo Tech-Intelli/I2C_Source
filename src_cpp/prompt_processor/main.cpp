6239647c37ed2bd056cb42356ebc71723672ec35
sayantanroy47 <sayantanroy47@gmail.com>
2024-07-19 18:32:20 +0200

    smol

--- src_cpp/prompt_processor/main.cpp
+++ src_cpp/prompt_processor/main.cpp
@@ -24,5 +24,16 @@ int main()
     );
     prompt.getPrompt(params);
 
+    PromptParams params1 = PromptParams(SocialMedia::TWITTER, // social media platform to use
+                                        "visual description", // visual description of the content
+                                        "context",            // context of the content
+                                        3,                    // limit on the number of hashtags to include in the prompt
+                                        CaptionSize::SMALL,   // caption size
+                                        "tone",               // tone of the content
+                                        "style",
+                                        Personas::BEAUTY_GURU // style of the content
+    );
+    prompt.getPrompt(params1);
+
     return 0;
 }
