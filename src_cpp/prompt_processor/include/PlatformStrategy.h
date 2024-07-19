435a11048f5c0a543edeab878a13c2a1a9f920e1
sayantanroy47 <sayantanroy47@gmail.com>
2024-07-19 18:32:33 +0200

    smol

--- src_cpp/prompt_processor/include/PlatformStrategy.h
+++ src_cpp/prompt_processor/include/PlatformStrategy.h
@@ -13,12 +13,13 @@ public:
     ~PlatformStrategy() = default;
     PlatformStrategy(std::string_view platform);
     std::string generatePrompt(const std::unordered_map<std::string, std::string> &replacementsMap);
-    void loadTemplate(const std::string &file_path, std::string &templateData);
+
+private:
     std::unique_ptr<PromptTemplateParser> template_str;
     std::string filepath;
     std::string templateData;
     std::string getFilePath(const std::string_view &platform);
 
-private:
+    void loadTemplate(const std::string &file_path, std::string &templateData);
     void initialize(const std::string_view &platform);
 };
